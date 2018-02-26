869fcfcdd67a3193c08d3832652588123fbbdf60
Hamilton Multimedia <24661651+HamiltonMultimedia@users.noreply.github.com>
2018-02-26 13:32:24 -0500

    Installed React Native Web

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Text, View } from 'react-native';
 import './App.css';
 
 class App extends Component {
@@ -6,7 +7,7 @@ class App extends Component {
     super();
     this.state = {
       projects: []
-    }
+    };
   }
   componentDidMount() {
     let projectsURL = 'http://localhost:8888/wp-json/wp/v2/projects';
@@ -14,30 +15,32 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         this.setState({
-            projects : response
-        })
-      })
+          projects: response
+        });
+      });
   }
 
   render() {
     let projects = this.state.projects.map((project, index) => {
       return (
         <div key={index}>
-          < img src = {
-            project.better_featured_image.media_details.sizes.medium.source_url
+          <img
+            src={
+              project.better_featured_image.media_details.sizes.medium
+                .source_url
             }
-            alt={ project.better_featured_image.alt_text }
+            alt={project.better_featured_image.alt_text}
           />
         </div>
-      )
-    })
+      );
+    });
     return (
-      <div className="App">
-
-          <h1 className="App-title">Projects</h1>
-          { projects }
-
-      </div>
+      <View style={{ flex: 1, backgroundColor: 'blue' }}>
+        <div className="App">
+          <h1 className="App-title" style={{ color: 'white'}} >Projects</h1>
+          {projects}
+        </div>
+      </View>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { AppRegistry } from 'react-native';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+AppRegistry.registerComponent('App', () => App);
+AppRegistry.runApplication('App', {
+  rootTag: document.getElementById('root')
+});
+
+// ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
